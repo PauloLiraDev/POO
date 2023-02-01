0c5922502ed8e772b24611d076e0ea9f817cdcb5
PauloLira <paulocesar_cl@hotmail.com>
2023-02-01 10:49:43 -0300

    Att

---
+++ Classes-2.py
@@ -0,0 +1,20 @@
+"""- Classe -> Modelo do objeto do mundo real sendo representado computacionalmente.
+
+Imagine que você queira fazer um sistema para automatizar o controle das lâmpadas da sua casa.
+
+Classes podem conter:
+
+    - Atributos -> Representam as características do objeto. Ou seja, pelos atributos conseguimos representar
+    computacionalmente os estados de um objeto. No caso da lâmpada, possivelmente iríamos querer saber a voltagem da
+    lâmpada, a cor da luz, qual é a luminosidade dela, etc.
+
+    - Métodos (funções) -> Representam os comportamentos do objeto. Ou seja, as ações que este objeto pode realizar no
+    seu sistema. No caso da lâmpada, por exemplo, um comportamento comum que muito provavelmente iríamos querer
+    representar é o de ligar/desligar.
+
+"""
+
+
+class Lampada:
+    pass
+
--- POO-1.py
+++ POO-1.py
@@ -14,6 +14,7 @@ Principais elementos da Orientação a Objetos
 
 """
 
+
 class Produto:
     pass
 
